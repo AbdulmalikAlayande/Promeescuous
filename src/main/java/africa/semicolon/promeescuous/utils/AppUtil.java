f52eb32e592edb9155034c1c075e4e1679cad370
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-08-08 14:38:41 +0100

    initial commit

--- src/main/java/africa/semicolon/promeescuous/utils/AppUtil.java
+++ src/main/java/africa/semicolon/promeescuous/utils/AppUtil.java
@@ -1,11 +1,16 @@
 package africa.semicolon.promeescuous.utils;
 
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
 import java.util.List;
 
 public class AppUtil {
@@ -19,7 +24,14 @@ public class AppUtil {
 		return String.join("", templateFileLines);
 	}
 	
-	public static String generateActivationLink(){
+	public static String generateActivationLink(String email){
+		JWT.create()
+				.withClaim("user mail", email)
+				.withExpiresAt(Instant.now().plusSeconds(3600))
+				.withIssuer("Promiscuous Incorporation")
+				.withIssuedAt(Instant.now())
+				.withHeader("")
+				.sign(Algorithm.HMAC512("secret"));
 		return "";
 	}
 }
